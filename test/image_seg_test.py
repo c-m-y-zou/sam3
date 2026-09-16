2278509f829b8c9ccce2d04569189553796c193a
c-m-y-zou <218041195+c-m-y-zou@users.noreply.github.com>
2026-09-16 14:44:08 +0900

    Add SAM3 image segmentation test script
    Ignore local test_data/ directory used for personal test images.

---
+++ test/image_seg_test.py
@@ -0,0 +1,46 @@
+
+import torch
+#################################### For Image ####################################
+from PIL import Image
+from sam3.model_builder import build_sam3_image_model
+from sam3.model.sam3_image_processor import Sam3Processor
+
+# turn on tfloat32 for Ampere+ GPUs, and use bfloat16 for the whole script
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+
+# Load the model
+model = build_sam3_image_model()
+processor = Sam3Processor(model)
+# Load an image
+image = Image.open("/home/y-zou/kajima_sam3/sam3/test_data/スクリーンショット 2026-09-15 175021.png").convert("RGB")
+inference_state = processor.set_image(image)
+# Prompt the model with text
+output = processor.set_text_prompt(state=inference_state, prompt="clamps")
+
+# Get the masks, bounding boxes, and scores
+masks, boxes, scores = output["masks"], output["boxes"], output["scores"]
+
+#################################### For Video ####################################
+
+# from sam3.model_builder import build_sam3_video_predictor
+
+# video_predictor = build_sam3_video_predictor()
+# video_path = "<YOUR_VIDEO_PATH>" # a JPEG folder or an MP4 video file
+# # Start a session
+# response = video_predictor.handle_request(
+#     request=dict(
+#         type="start_session",
+#         resource_path=video_path,
+#     )
+# )
+# response = video_predictor.handle_request(
+#     request=dict(
+#         type="add_prompt",
+#         session_id=response["session_id"],
+#         frame_index=0, # Arbitrary frame index
+#         text="<YOUR_TEXT_PROMPT>",
+#     )
+# )
+# output = response["outputs"]
